55c6bed2532226677614d7ad4a9e5cd4a3780b93
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-07-04 20:55:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/multipleactivities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void nextActivity (View view){
+
+        Intent intent = new Intent(getApplicationContext(),secondActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/multipleactivities/secondActivity.java
@@ -0,0 +1,22 @@
+package com.example.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class secondActivity extends AppCompatActivity {
+
+
+    public void backButton(View view){
+//        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//        startActivity(intent);
+        finish();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
